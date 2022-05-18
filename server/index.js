c2733edc4660c03a9db28fc998fd0826802c010d
danehermansen <daneherm211@gmail.com>
2022-05-18 12:30:43 -0600

    second commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 //Endpoints
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 console.log(__dirname)
